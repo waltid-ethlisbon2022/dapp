3711af4c3512b328e1931def182238306cacff99
Philipp Potisk <phil@walt.id>
2022-10-29 23:02:00 +0100

    protocol working

--- pages/index.vue
+++ pages/index.vue
@@ -10,13 +10,24 @@
                 <div class="row py-lg-5">
                     <div class="col-lg-6 col-md-8 mx-auto">
                         <img class="p-5" src="https://walletconnect.com/images/logo.svg" alt="Logo" />
-                        <p >
-                            <a :href="'/xdevice/' + encodeURIComponent(vidSchemaUri)"
-                                class="wallect-connect-btn btn btn-primary  my-2 fw-bold _btn">Connect Wallet using SSI over WalletConnect</a>
-                        </p>
-                                                         
+
+                                                                                 
                         <p class="lead connect-txt mt-6">
                             Connect your wallet and share<br>your credentials to access services. </p>
+
+                        <p >
+                            <a :href="'/xdevice/'"
+                                class="wallect-connect-btn btn btn-primary  my-2 fw-bold _btn">Connect Wallet using SSI over WalletConnect</a>
+                        </p>
+
+
+                        <div class="protocolLog mt-3">
+
+                            <ul>
+                                <li v-for="line in protocolLog">{{ line }}</li>
+                            </ul>
+                                
+                        </div>           
                      
                         <button class="btn btn-primary" @click="initWalletConnectChat">Start Client</button>
                         <p class="text-muted fw-bold">© 2022 walt.id</p>
@@ -28,6 +39,7 @@
 </template>
 
 <script>
+import { INVALID_REQUEST } from '@walletconnect/jsonrpc-utils';
 import {config} from '/config.js'
 
 if (typeof window !== "undefined")
@@ -37,9 +49,7 @@ const ChatClient = require("@walletconnect/chat-client").ChatClient
 export default {
     data() {
         return {
-            vidSchemaUri1: "https://ngiatlantic.info/schema/V-2022-1/OpenBadgeCredential.json",
-            vidSchemaUri: 'https://api.preprod.ebsi.eu/trusted-schemas-registry/v1/schemas/0xb77f8516a965631b4f197ad54c65a9e2f9936ebfb76bae4906d33744dbcc60ba',
-            bidSchemaUri: 'https://raw.githubusercontent.com/walt-id/waltid-ssikit-vclib/master/src/test/resources/schemas/EuropeanBankIdentity.json'
+            protocolLog: ["App loaded ..."]
         }
     },
     async asyncData({ $axios }) {
@@ -53,7 +63,9 @@ export default {
         initWalletConnectChat: async function() {
 
             let dappAccount = `eip155:1:${config.ethAccount}`
-            console.log(`Initializing WalletConnect Chat Client for ${dappAccount} ...`)
+
+            this.protocolLog.push(`Initializing WalletConnect Chat Client for ${dappAccount}`)
+            console.log(this.protocolLog)
 
             console.log(config.walletConnectId)
         
@@ -61,7 +73,48 @@ export default {
                 projectId: config.walletConnectId
             })
 
-            await chatClient.register({ account: `${dappAccount}` });
+            const retReg = await chatClient.register({ account: dappAccount });
+
+            this.protocolLog.push(`Register client returned: ${retReg}`)
+            console.log(this.protocolLog);
+
+            chatClient.on("chat_invite", async (event) => {
+                this.protocolLog.push("Client received invite");
+                
+                console.log(event)
+        
+                const topic = await chatClient.accept({id: event.id}).catch(e => {console.error(e)})
+
+                this.protocolLog.push(`Client accepted. topic: ${topic}`);
+
+                await chatClient.message({
+                    topic: topic, // Topic of the chat thread this message should be sent to.
+                    payload: {
+                        message: "VerifiableId", // the message you want to send.
+                        authorAccount: dappAccount, // your CAIP-2 formatted account that you registered previously.
+                        timestamp: Date.now(),
+                    },
+                }).catch(e => {console.error(e)});
+                
+            });
+
+            chatClient.on("chat_joined", async (event) => {
+                console.log("Client joined");
+            });
+
+            chatClient.on("chat_ping", async (event) => {
+                console.log("chat_ping joined");
+            });
+
+
+            chatClient.on("chat_message", async (event) => {
+                console.log(event);
+                console.log( "Credential received: " + event.params.message);
+                this.protocolLog.push("Credential received: " + event.params.message);
+               // const sessionInfo = await this.$axios.$get("/api/wallet/presentation/create?type=" + event.params.message)
+                //this.$router.replace("/CredentialRequest/?sessionId=" + sessionInfo.id)
+                
+            });
 
         }
     }
@@ -85,8 +138,13 @@ export default {
 }
 
 .wallect-connect-btn{
-
     background-color: transparent;
      border-color: white!important;
+}
+
+.protocolLog {
+    background-color: black;
+    color:greenyellow;
+    text-align: left;
 }
 </style>
